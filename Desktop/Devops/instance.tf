e97c5ed65f0e46588d6fd6a5f4f953709ecb97a1
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-02-04 19:06:48 +0530

    ami updated

--- Desktop/Devops/instance.tf
+++ Desktop/Devops/instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" 
 }
 resource "aws_instance" "myec2" {
-    ami = "ami-01816d07b1128cd2d"
+    ami = "ami-04b4f1a9cf54c11d0"
     instance_type = "t2.micro"
     key_name = "teraaform-1"
     security_groups = [ "default" ]
