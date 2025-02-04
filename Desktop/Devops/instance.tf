f79bffb9297afa813508b4e0b97ce4cb7253dae6
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-02-04 17:01:09 +0530

    changes updated 1

--- Desktop/Devops/instance.tf
+++ Desktop/Devops/instance.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "myec2" {
     ami = "ami-01816d07b1128cd2d"
     instance_type = "t2.micro"
     key_name = "teraaform-1"
+    security_groups = [ "default" ]
     tags = {
       Name = "spiderinstance"
     }
