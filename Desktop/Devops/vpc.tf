3575cecabc88bf426b792aaac5cad61133abcf6f
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-02-04 19:40:25 +0530

    vpc created with subnet, route-table and igw

---
+++ Desktop/Devops/vpc.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MyVPC-1"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "MySubnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyIGW-1"
+  }
+}
+
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "MyRouteTable-1"
+  }
+}
