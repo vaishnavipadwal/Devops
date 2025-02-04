1a1753a26a7650c1b3ff97b8195f97b00b0e0bf3
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-02-04 16:53:25 +0530

    changes updated

--- Desktop/Devops/instance.tf
+++ Desktop/Devops/instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" 
 }
 resource "aws_instance" "myec2" {
-    ami = "ami-09a9858973b288bdd"
+    ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
     key_name = "teraaform-1"
     tags = {
