d9c7072d9dc8fe7f69d2c4ddfa9b4917a7a2b126
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-02-11 00:30:07 +0530

    push with commemts

--- Desktop/Devops/static website/main.tf
+++ Desktop/Devops/static website/main.tf
@@ -1,8 +1,11 @@
-provider "aws" {
+#Specifies AWS as the provider and sets the region to "us-east-1"
+provider "aws" { 
   region = "us-east-1"  
 }
 
-resource "aws_s3_bucket" "website1" {
+
+#Creates an S3 bucket named "my-static-website-bucket-00001" with tags "Name": "MyStaticWebsite-1" and "Environment": "Dev".
+resource "aws_s3_bucket" "website1" { 
   bucket = "my-static-website-bucket-00001"
 
   tags = {
@@ -11,6 +14,9 @@ resource "aws_s3_bucket" "website1" {
   }
 }
 
+
+
+#Configures the S3 bucket to host a static website, using "index.html" as the default page.
 resource "aws_s3_bucket_website_configuration" "website-1" {
   bucket = aws_s3_bucket.website1.id
 
@@ -20,6 +26,8 @@ resource "aws_s3_bucket_website_configuration" "website-1" {
 
 }
 
+
+#Sets a public access policy for the S3 bucket to allow everyone to read the objects in it (e.g., allow access to "index.html")
 resource "aws_s3_bucket_policy" "public_access" {
   bucket = aws_s3_bucket.website1.id
 
@@ -39,7 +47,7 @@ resource "aws_s3_bucket_policy" "public_access" {
 EOF
 }
 
-
+#Modifies the S3 bucket's public access settings to allow public access, despite other public access blocks.
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.website1.id
 
@@ -49,6 +57,8 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = false
 }
 
+
+#Uploads a file called "index.html" from your local system to the S3 bucket, setting its content type to "text/html".
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.website1.id
   key    = "index.html"
